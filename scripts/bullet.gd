63039891a1502e198351462a19ce4622b2e63aac
Emanuel <emar092009@gmail.com>
2026-09-15 21:09:53 -0500

    shooting system added

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,6 +4,12 @@ var direction_bullet: float = 0.0
 var speed_bullet: float = 1000.0
 
 func _ready():
-	rotation=direction_bullet
+	rotation = direction_bullet
+
 func _physics_process(delta: float):
-	velocity= Vector2(speed_bullet,0).rotated(direction_bullet)
+	velocity = Vector2(speed_bullet, 0).rotated(direction_bullet)
+	move_and_slide() # ¡NUEVO: Obligatorio para que los CharacterBody2D se muevan!
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- scripts/trauma_boy.gd
+++ scripts/trauma_boy.gd
@@ -2,10 +2,23 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+var bullet = preload("res://scenes/bullet.tscn")
+var is_shooting: bool = false
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
+	# ELIMINADO: look_at(get_last_motion()) ya no es necesario aquí
+	
+	if Input.is_action_just_pressed("shoot"):
+		is_shooting = true
+		anim.play("shoot")
+		shoot()
+		
+		await anim.animation_finished
+		is_shooting = false
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -26,8 +39,23 @@ func _physics_process(delta: float) -> void:
 	actualizar_animaciones(direction)
 
 	move_and_slide()
-
+	
+func shoot():
+	var newBullet = bullet.instantiate()
+	
+	if anim.flip_h:
+		newBullet.direction_bullet = PI
+		
+		newBullet.global_position = global_position + Vector2(-abs($bulletspawn.position.x), $bulletspawn.position.y)
+	else:
+		newBullet.direction_bullet = 0.0 # Derecha
+		newBullet.global_position = $bulletspawn.global_position
+		
+	get_parent().add_child(newBullet)
+	
 func actualizar_animaciones(direction: float) -> void:
+	if is_shooting:
+		return
 	if not is_on_floor():
 		anim.play("jump")
 	else:
