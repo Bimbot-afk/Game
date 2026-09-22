98dbb276dc5e4009084438f4c12fa1b199523081
Emanuel <emar092009@gmail.com>
2026-09-21 22:23:09 -0500

    bug fix of win scene

--- scripts/end_game.gd
+++ scripts/end_game.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	# 1. Filtra para que SOLO afecte al jugador (asumiendo que tu personaje se llama "Player")
-	if not body.name.to_lower().contains("player"):
-		return # Si no es el jugador, ignora el objeto y no hace nada
 
-	# 2. Ahora que sabemos que es el jugador, revisamos las condiciones de victoria
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if not area.name.to_lower().contains("trauma"):
+		return
+
 	if GlobalCoin.coin >= 80:
-		get_tree().change_scene_to_file("res://scenes/win.tscn")
+		get_tree().change_scene_to_file("res://scenes/win.tscn") # Replace with function body.
